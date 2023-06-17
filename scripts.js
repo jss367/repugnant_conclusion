11af9b9081dc203907519795230187ded8cd46d8
Julius Simonelli <julius.simonelli@gmail.com>
2023-06-16 19:11:49 -0700

    Create scripts.js

---
+++ scripts.js
@@ -0,0 +1,27 @@
+// Here is a placeholder for the data. You'll want to replace this
+// with data about the different populations.
+var populations = [
+    {
+        "name": "Population A",
+        "description": "Description of population A..."
+    },
+    {
+        "name": "Population B",
+        "description": "Description of population B..."
+    }
+];
+
+// Function to display the population choices to the user
+function displayChoices() {
+    // Use JavaScript to manipulate the DOM and present the choices to the user.
+    // You can use the 'populations' variable above to get the data.
+}
+
+// Function to handle the user's choice
+function handleChoice(choice) {
+    // Update the scenario based on the user's choice.
+    // Display an explanation of what their choice means in the context of the Repugnant Conclusion.
+}
+
+// Call the displayChoices function when the page loads
+window.onload = displayChoices;
