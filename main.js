fbd94ea4fba51cce423a66fa5605ec584acdf89e
Julius <julius.simonelli@gmail.com>
2023-06-30 21:47:15 -0700

    Update main.js

--- main.js
+++ main.js
@@ -1,76 +1,79 @@
-// Initialize the populations
+// Initialize the universes
 let universes = [
-  [
-    {
-        name: "Group A1",
-        quantity: 10,
-        welfare: 100
-    }
-  ],
-  [
-    {
-        name: "Group B1",
-        quantity: 10,
-        welfare: 100
-    },
-    {
-        name: "Group B2",
-        quantity: 20,
-        welfare: 90
-    }
-  ]
+  {
+      name: "Universe A",
+      description: "Universe A includes a group of people with high welfare...",
+      populations: [
+        {quantity: 10, welfare: 100}
+      ]
+  }
 ];
 
 // Function to display the universe choices to the user
 function displayChoices() {
+  // Clear previous display
+  document.getElementById('simulation').innerHTML = '';
+  
   for (let i = 0; i < universes.length; i++) {
       let universe = universes[i];
-      let universeName = String.fromCharCode(65 + i);
-      let universeDescription = "This universe contains the following groups:\n";
+
+      // Create HTML elements for the universe
+      let universeElement = document.createElement('div');
+      universeElement.className = 'universe';
+      universeElement.innerHTML = `
+        <h3>${universe.name}</h3>
+        <p>${universe.description}</p>
+        <div class="rectangle-container" id="rectangle-container${i}">
+        </div>
+        <button id="choose${i}">Choose this universe</button>
+      `;
       
-      universe.forEach((population, index) => {
-        universeDescription += `${population.name}: ${population.quantity} people, welfare ${population.welfare}\n`;
-        
-        // Update the rectangle
-        let rectangle = document.getElementById(`rectangle${universeName}${index + 1}`);
-        rectangle.style.width = `${population.quantity}px`;
-        rectangle.style.height = `${population.welfare}px`;
-        
-        // Update the welfare and quantity values
-        document.getElementById(`welfare${universeName}${index + 1}`).innerText = population.welfare;
-        document.getElementById(`quantity${universeName}${index + 1}`).innerText = population.quantity;
-      });
-      
-      // Update the name, description, and button
-      document.getElementById(`name${universeName}`).innerText = `Universe ${universeName}`;
-      document.getElementById(`description${universeName}`).innerText = universeDescription;
-      document.getElementById(`choose${universeName}`).addEventListener('click', () => handleChoice(universe));
+      // Add the universe element to the simulation section
+      document.getElementById('simulation').appendChild(universeElement);
+
+      // Add an event listener to the button
+      document.getElementById(`choose${i}`).addEventListener('click', () => handleChoice(universe));
+
+      // Add rectangles for the populations
+      for (let j = 0; j < universe.populations.length; j++) {
+          let population = universe.populations[j];
+          let rectangle = document.createElement('div');
+          rectangle.className = 'rectangle';
+          rectangle.style.width = `${population.quantity}px`;
+          rectangle.style.height = `${population.welfare}px`;
+          document.getElementById(`rectangle-container${i}`).appendChild(rectangle);
+      }
   }
 }
 
 // Function to handle the user's choice
 function handleChoice(choice) {
-  document.getElementById('choiceDisplay').innerText = `You chose ${choice.name}, which has a high welfare group of ${choice.populations[0].quantity} people and a lower welfare group of ${choice.populations[1].quantity} people.`;
+  document.getElementById('choiceDisplay').innerText = `You chose ${choice.name}.`;
 
-  // Generate the next universe to be displayed after the chosen universe
-  if(choice.name !== 'Universe A') {
-    let lastUniverseLetter = choice.name.slice(-1);
-    let nextUniverseLetter = String.fromCharCode(lastUniverseLetter.charCodeAt(0) + 1);
-    let newUniverse = {
-      name: `Universe ${nextUniverseLetter}`,
-      description: `Universe ${nextUniverseLetter} is a larger population with slightly lower welfare...`,
-      populations: [
-        {quantity: choice.populations[0].quantity + choice.populations[1].quantity, welfare: choice.populations[1].welfare - 5}
-      ]
-    };
-    
-    populations = [choice, newUniverse];
+  // Generate the next universe
+  let lastUniverseLetter = choice.name.slice(-1);
+  let nextUniverseLetter = String.fromCharCode(lastUniverseLetter.charCodeAt(0) + 1);
+  let newNumPpl, newHappiness;
+  if (choice.populations.length > 1) {
+    // If the universe has two populations, combine them
+    newNumPpl = [choice.populations[0].quantity + choice.populations[1].quantity];
+    newHappiness = [(choice.populations[0].welfare + choice.populations[1].welfare) / 2];
+  } else {
+    // If the universe has one population, split it into two
+    newNumPpl = [choice.populations[0].quantity, 2 * choice.populations[0].quantity];
+    newHappiness = [choice.populations[0].welfare, choice.populations[0].welfare * 0.9];
   }
+  
+  let newUniverse = {
+    name: `Universe ${nextUniverseLetter}`,
+    description: `Universe ${nextUniverseLetter} includes a group of people with welfare...`,
+    populations: newNumPpl.map((quantity, index) => ({quantity: quantity, welfare: newHappiness[index]}))
+  };
+  universes = [choice, newUniverse];
   
   // Update the display
   displayChoices();
 }
-
 
 // Call the displayChoices function when the page loads
 window.onload = displayChoices;
